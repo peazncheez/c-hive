af400bab6c8c337dc53415680a616ca236a9dc83
Bryan Williams <2016bmw@mit.edu>
2020-07-01 13:26:57 -0700

    Initial Commit

---
+++ c-hive/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-hive
+//
+//  Created by Bryan Williams on 7/1/20.
+//  Copyright © 2020 Bryan Williams. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
